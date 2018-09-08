4c5b662ef0a0844bab1fee5109eaef763c22c097
ruanwenjun <861923274@qq.com>
2018-09-08 21:39:02 +0800

    update

--- httpclientscraw/src/main/java/com/ruanwenjun/scrawmain.java
+++ httpclientscraw/src/main/java/com/ruanwenjun/scrawmain.java
@@ -38,73 +38,7 @@ public class scrawmain {
         queryList.addAll(baiduPeopleFeature.get());
         queryList.addAll(baiduStarFeature.get());
         queryList.addAll(baiduSportFeature.get());
-        //extend(queryList);
         //JDBCUtil.insert(queryList);
-    }
-
-
-    public static void extend(List<ScrawEntity> list) {
-        // site,hot,set,count
-        for (ScrawEntity entity : list) {
-            try {
-                List<WordCount> search = search(entity.getHot());
-                Set<String> oriset = entity.getWords();
-                System.out.println(search);
-                System.out.println(oriset);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    /**
-     * 在微博搜索
-     *
-     * @param searchkey
-     * @throws IOException
-     */
-    public static List<WordCount> search(String searchkey) throws IOException {
-        List<WordCount> reverseList = new ArrayList<>();
-        String fullPath = "http://s.weibo.com/list/relpage?search=" + searchkey.replace(" ", "");
-        System.out.println(fullPath);
-        HttpGet request = new HttpGet(fullPath);
-        //request.setConfig(config);
-        request.setHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36");
-        HttpResponse response = HttpClients.createDefault().execute(request);
-        String content = EntityUtils.toString(response.getEntity());
-        String[] split = content.split("\n");
-        for (String line : split) {
-            if (line.contains("<script>STK && STK.pageletM && STK.pageletM.view({\"pid\":\"pl_list_relpage\"")) {
-                int index = line.indexOf("html\":\"");
-                if (index > 0) {
-                    String substring = line.substring(index + 7, line.length());
-                    Document parse = Jsoup.parse(WordUtil.convertUnicode(substring));
-                    List<String> titles = parse.select("p.link_title2").select("a").eachAttr("title");
-                    HashMap<String, Integer> map = new HashMap<>();
-                    for (String title : titles) {
-                        List<String> hots = WordUtil.sentenceProcess(title);
-                        for (String hot : hots) {
-                            map.put(hot, map.getOrDefault(hot, 0) + 1);
-                        }
-                    }
-                    List<WordCount> wordCounts = new ArrayList<>();
-                    for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                        WordCount wordCount = new WordCount();
-                        wordCount.setWord(entry.getKey());
-                        wordCount.setNum(entry.getValue());
-                        wordCounts.add(wordCount);
-                    }
-                    wordCounts.sort(Comparator.comparingInt(WordCount::getNum));
-                    if (wordCounts.size() > 20) {
-                        reverseList = wordCounts.subList(0, 20);
-                    } else {
-                        reverseList = wordCounts;
-                    }
-                }
-            }
-        }
-        return reverseList;
     }
 
 
